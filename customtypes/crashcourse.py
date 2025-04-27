beb903818bb8d8d037d6ef92fa6beab3f1f84a26
stormzzy900 <stormzzy900@gmail.com>
2025-04-27 20:55:44 +0000

    update

---
+++ customtypes/crashcourse.py
@@ -0,0 +1,49 @@
+import re
+from dataclasses import dataclass
+from pydantic import BaseModel, Field
+from rich.panel import Panel
+from rich.console import Group, Console
+console = Console()
+
+@dataclass
+class LanguageCodePair:
+    lang: str
+    code: str
+
+class CrashCourse(BaseModel):
+    title :str = Field(description="A very simple, very brief title of the crash course. ")
+    keypoints: list[str] = Field(description="A list of keypoints to get up and running. ")
+
+    @staticmethod
+    def prompt(module:str):
+        return f"""Generate a crash course for how to use python module:{module}. Go over the basics to get up and running quickly.
+        In your keypoints, favor writing succint keypoints (3-4) and favor adding code and terminal commands.
+        Things to note:
+        don't use single backticks (`) for code or terminal commands, only use triple backticks (```)
+        You are primarily concerned about python packages"""
+
+    def __str__(self):
+        return f"""
+        Keypoints: {self.keypoints} 
+        """
+    
+    def print(self):
+        panel = Panel(str(self), title=self.title)
+        print(panel)
+    
+    def getCodeBlocks(self) -> LanguageCodePair:
+        regex = r"```(\w+)\n(.+)```"
+        codeblocks = []
+        for keypoint in self.keypoints:
+            matches = re.findall(regex, keypoint, re.DOTALL)
+            for language, code in matches:
+                pair = LanguageCodePair(language, code)
+                codeblocks.append(pair)
+        return codeblocks
+    
+    def printCodeBlocks(self) -> None:
+        codeblocks : LanguageCodePair = self.getCodeBlocks()
+        groups : Group = Group(*[Panel(codeblock.code, title=codeblock.lang) for codeblock in codeblocks])
+        panel : Panel = Panel(groups, title="Code Blocks")
+        console.print(panel)
+   
--- main.py
+++ main.py
@@ -1,52 +1,30 @@
 import typer
-import importlib
 from env import Env
 from genkit.ai import Genkit
 from genkit.plugins.google_genai import GoogleAI
-from pydantic import BaseModel, Field
-from typing import Type, Any
-from customtypes.keypoint import KeyPoint
 from rich import print
-from rich.panel import Panel
+from customtypes.crashcourse import CrashCourse
 
 app = typer.Typer()
 Env()
 ai = Genkit(plugins=[GoogleAI()], model="googleai/gemini-2.0-flash")
-
-class CrashCourse(BaseModel):
-    title :str = Field(description="A very simple, very brief title of the crash course. ")
-    keypoints: list[str] = Field(description="A list of keypoints to get up and running. ")
-
-    @staticmethod
-    def prompt(module:str):
-        return f"Generate a crash course for how to use python module:{module}. Go over the basics to get up and running quickly.In your keypoints, favor writing succint keypoints (3-4) and favor adding code and terminal commands"
-
-    def __str__(self):
-        return f"""
-        Keypoints: {self.keypoints} 
-        """
-    
-    def print(self):
-        panel = Panel(str(self), title=self.title)
-        print(panel)
-        
+     
 async def generate_answer(prompt:str) :
     result = await ai.generate(prompt=prompt, output_schema=CrashCourse)
     cc: CrashCourse =  CrashCourse.model_validate(result.output)
     cc.print()
+    cc.printCodeBlocks()
     
-
-
 @app.command()
 def crashcourse(python_module:str):
     """
-    Dynamically import a Python module and print its docstring.
+    Gen a crashcourse.
     """
-    try:
-        module = importlib.import_module(python_module)
-        print(module.__doc__)
-    except ImportError:
-        print(f"Module '{python_module}' not found.")
+    # try:
+    #     module = importlib.import_module(python_module)
+    #     print(module.__doc__)
+    # except ImportError:
+    #     print(f"Module '{python_module}' not found.")
     ai.run_main(generate_answer(CrashCourse.prompt(python_module)))
     
         
