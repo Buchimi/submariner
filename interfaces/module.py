9a3aa9e1aa54d6b765a616b490e214de77511816
stormzzy900 <stormzzy900@gmail.com>
2025-05-05 03:44:23 +0000

    add support for functions

--- interfaces/module.py
+++ interfaces/module.py
@@ -4,10 +4,51 @@ from .pypi import PyPi
 from rich.console import Console
 from types import ModuleType, FunctionType
 from typing import Type
+import inspect
 
 console = Console()
+class Promptable:
+    def prompt(self, goal: str | None = None) -> str:
+        raise NotImplementedError("prompt not implemented")
+    
+    @staticmethod
+    def to_promptable(obj:object) -> "Promptable":
+        """
+        This will convert a object to one of the promptable subtypes.
+        TODO: See if this function should be in the promptable class or as a member function of this module
+        """
+        if isinstance(obj, Promptable):
+            return obj
+        elif isinstance(obj, FunctionType):
+            return Function(obj)
+        elif isinstance(obj, ModuleType):
+            return Module._from_module(obj)
+        elif isinstance(obj, Type):
+            raise NotImplementedError("Classes are not yet supported")
+        else:
+            raise TypeError(f"Entity {obj} is not a module, function or class")
 
-class Module:
+class Function(Promptable):
+    def __init__(self, function:FunctionType) -> None:
+        self.function = function
+        self.name = function.__name__
+        self.args = inspect.signature(function).parameters
+        self.docstring = function.__doc__
+        self.module = Module(function.__module__)
+
+    def __str__(self) -> str:
+        return f"{self.name}({self.args})"
+    
+    def __call__(self, *args, **kwargs):
+        return self.function(*args, **kwargs)
+    
+    def __repr__(self):
+        return self.__str__()
+
+    def __doc__(self) -> str:
+        return self.docstring
+    
+class Module(Promptable):
     def __init__(self, module:str) -> None:
         try:
             self.module = importlib.import_module(module)
@@ -42,7 +83,7 @@ class Module:
         
     def submodules(self) -> list[ModuleType]:
         all_attributes = self._get_all_attributes()
-        submodules =  [getattr(self.module, item) for item in all_attributes if isinstance(getattr(self.module, item), ModuleType)]
+        submodules =  [getattr(self.module, item) for item in all_attributes if isinstance(getattr(self.module, item), ModuleType) and getattr(getattr(self.module, item), "__name__").startswith(self.module.__name__)]
         return [Module._from_module(mod) for mod in submodules]
     
     def get_submodule(self, module_name:str) -> ModuleType:
@@ -58,7 +99,46 @@ class Module:
         else:
             raise ValueError(f"Module {module_name} not found")
     
-    
-    
+    def resolve_attribute(self, attr:str) -> Promptable:
+        if hasattr(self.module, attr):
+            entity = getattr(self.module, attr)
+            return Promptable.to_promptable(entity)
+        else:
+            members = self.members()
+            for member in members:
+                if hasattr(member, attr):
+                    return Promptable.to_promptable(getattr(member, attr))
+            raise ValueError(f"Entity {attr} not found")
 
+    def classes(self) -> list[Type]:
+        all_attributes = self._get_all_attributes()
+        classes =  [getattr(self.module, item) for item in all_attributes if isinstance(getattr(self.module, item), type)]
+        return classes
     
+    def functions(self) -> list[Function]:
+        all_attributes = self._get_all_attributes()
+        functions =  [Function(getattr(self.module, item)) for item in all_attributes if isinstance(getattr(self.module, item), FunctionType)]
+        return functions
+    
+    def members(self):
+        return self.functions() + self.classes()
+    
+    def __str__(self) -> str:
+        return f"Module: {self.module.__name__}" + "\n"+ f"{self.module.__doc__.splitlines()[0] if self.module.__doc__ else ''}"
+    def __doc__(self) -> str:
+        return self.module.__doc__ if self.module.__doc__ else str(self.module)
+    
+    def __repr__(self) -> str:
+        return self.__str__()
+
+    def prompt(self, goal: str | None = None, be_brief:bool = True) -> str:
+        prompt_builder = []
+        if goal:
+            prompt_builder.append(f"""Tell me how the python module: {str(self.module)} could be used to achieve the goal: {goal}""")
+        else:
+
+            prompt_builder.append(f"""Explain the use of the python module: {str(self.module)}. What it's for and what members I am likely to use""")
+        if be_brief:
+            prompt_builder.append("Be very brief in your analysis, as though you were a cli tool or a programmer with a short attention span")
+        
+        return "\n".join(prompt_builder)
--- main.py
+++ main.py
@@ -17,7 +17,11 @@ async def generate_answer(prompt:str) :
     result = await ai.generate(prompt=prompt, output_schema=CrashCourse)
     cc: CrashCourse =  CrashCourse.model_validate(result.output)
     cc.printCodeBlocks()
-    
+
+async def generate_deepdive_answer(prompt:str) :
+    result = await ai.generate(prompt=prompt, )
+    print(result.text)
+
 @app.command()
 def spark(python_module:str):
     """
@@ -26,19 +30,32 @@ def spark(python_module:str):
     ai.run_main(generate_answer(CrashCourse.prompt(python_module)))
     
 @app.command()
-def deepdive(module_str:str):
+def deepdive(module_str:str, use_ai: bool = False, goal: str | None = None):
     """
     Describe a function or class in a python module and how it could be used.
     """ 
     # try to import the module
-    module = Module(module_str)
+    module_str_split = module_str.split(".")
+    start = 0
+    module = Module(module_str_split[start])
+    start = 1
+    while start < len(module_str_split):
+        module_str = module_str_split[start]
+        module = module.resolve_attribute(module_str)
+        if not isinstance(module, Module):
+            break
+        start +=1
     
-    if hasattr(module, "utils"):
-        utils = getattr(module, "utils")
-        print(utils)
-        print(dir(utils))
-            
-
+    if use_ai:
+        ai.run_main(generate_deepdive_answer(module.prompt(goal)))
+    else:
+        if isinstance(module, Module):
+            print(module)
+            for member in module.members():
+                print(member)
+            print(module.submodules())
+        else:
+            print(module)
 
 if __name__ == "__main__":
     app()
